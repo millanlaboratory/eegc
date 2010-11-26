5cca64886144c443a447526b41007cfb36d0e95a
Michele Tavella <tavella.michele@gmail.com>
2010-11-26 22:52:31 +0100

    Added script to convert eegc2 description.txt in eegc3 .log files

---
+++ scripts/description2log.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# 2010-11-26  Michele Tavella <michele.tavella@epfl.ch>
+
+ROOTFOLDER=$1
+
+if [ "z$ROOTFOLDER" == "z" ]; then
+	echo "Error: descrition2log.sh ROOTFOLDER";
+	exit 1;
+fi
+
+echo '[descrition2log] Root folder is: $ROOTFOLDER';
+
+for txtfile in `find  $ROOTFOLDER -iname "description.txt"`; do
+	dirname=`dirname $txtfile`;
+	name=`basename $dirname`;
+	basename=`echo $name | sed -e s/_/\ /g | awk '{print $2 "_" $1}'`;
+	logfile="$dirname/$basename.log"
+
+	echo "+- txtfile:  $txtfile"
+	echo "|- dirname:  $dirname"
+	echo "|- name:     $name"
+	echo "|- basename: $basename"
+	echo "\`- logfile:  $logfile"
+
+	# Using awk to reshuffle, should be enough.
+	# I extract up to 9 fields to be on the safe side.
+	cat $txtfile | \
+		awk '{print $3 " " $4 " " $5 " " $6 " " $7 " " $8 " " $9}' | \
+		tr [A-Z] [a-z] \
+		| sed -e s/analysis/classifier/g > $logfile
+done
+
+exit 0;
