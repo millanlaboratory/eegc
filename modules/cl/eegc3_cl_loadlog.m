0830867ba5f07556962e1348c612023909a6bf63
Michele Tavella <tavella.michele@gmail.com>
2010-11-27 01:08:46 +0100

    Added eegc2/eegc3 autodetection and added path to files

--- modules/cl/eegc3_cl_loadlog.m
+++ modules/cl/eegc3_cl_loadlog.m
@@ -1,5 +1,20 @@
 % 2009-12-08  Michele Tavella <michele.tavella@epfl.ch>
-function session = eegc3_smr_loadlog(filename)
+%
+% Example: 
+%    logfile = '~/Research/cnbi/mi/20101118_a2/a2_20101118.log';
+%    session = eegc3_cl_loadlog(logfile);
+function session = eegc3_cl_loadlog(filename)
+
+% Recsession.runs.all & harvest
+session = eegc3_cl_newsession();
+[session.base, session.path] = mtpath_basename(filename);
+session.base= strrep(session.base, '.log', '');
+[session.name, session.root] = mtpath_basename(session.path);
+
+cache = mt_strsplit('_', session.name);
+session.daytime = cache{1};
+session.subject = cache{2};
+
 
 printf('[eegc3_smr_loadlog] Loading: %s\n', filename);
 fid = fopen(filename, 'r');
@@ -15,10 +30,10 @@ end
 fclose(fid); 
 
 printf('[eegc3_smr_loadlog] Allocating stuctures:\n');
-all = {};
+cache = {};
 for i = 1:length(entries)
-	all{i}.file = cell2mat(entries{i}{1});
-	printf('  %-35.35s ', all{i}.file);
+	session.runs.all{i}.file = cell2mat(entries{i}{1});
+	printf('  %-35.35s ', session.runs.all{i}.file);
 	for j = 2:11
 		if(isempty(entries{i}{j}) == true)
 			if(j == 2)
@@ -30,7 +45,7 @@ for i = 1:length(entries)
 		cache.fields = mt_strsplit('=', cache.buffer);
 		cache.name = cache.fields{1};
 		cache.value = cache.fields{2};
-		cache.expression = ['all{i}.' cache.name ' = ''' cache.value ''';'];
+		cache.expression = ['session.runs.all{i}.' cache.name ' = ''' cache.value ''';'];
 		eval(cache.expression);
 		printf('%s ', cache.name);
 	end
@@ -39,31 +54,26 @@ end
 clear cache;
 
 printf('[eegc3_smr_loadlog] Detecting online/offline:\n');
-online = {};
-offline = {};
-for i = 1:length(all)
-	printf('  %-35.35s ', all{i}.file);
+for i = 1:length(session.runs.all)
+	printf('  %-35.35s ', session.runs.all{i}.file);
 	try
-		all{i}.classifier;
+		session.runs.all{i}.file =[session.path '/' session.runs.all{i}.file];
+		session.runs.all{i}.classifier =[session.path '/' session.runs.all{i}.classifier];
 	catch
-		offline{end+1} = all{i};
+		session.runs.offline{end+1} = session.runs.all{i};
 		printf('offline\n');
 		continue;
 	end
-	online{end+1} = all{i};
-	printf('online\n');
+	session.runs.online{end+1} = session.runs.all{i};
+	printf('online ');
+	try
+		session.runs.all{i}.frames;
+		session.trace.eegcversion = 2;
+		session.runs.all{i}.classifier = [session.runs.all{i}.classifier '.3'];
+		printf(' [eegc v2]');
+	catch
+		session.trace.eegcversion = 3;
+		printf(' [eegc v3]');
+	end
+	printf('%s\n', ' ');
 end
-
-% Recall & harvest
-session = eegc3_cl_newsession();
-[session.base, session.path] = mtpath_basename(filename);
-session.base= strrep(session.base, '.log', '');
-[session.name, session.root] = mtpath_basename(session.path);
-
-cache = mt_strsplit('_', session.name);
-session.daytime = cache{1};
-session.subject = cache{2};
-
-session.runs.all     = all;
-session.runs.online  = online;
-session.runs.offline = offline;
