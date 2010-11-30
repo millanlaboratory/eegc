75559d52d449d7b79386461ebe7e4f5ba55d3282
Michele Tavella <tavella.michele@gmail.com>
2010-11-30 11:15:39 +0100

    Fixed init

--- modules/smr/smr_init.m
+++ modules/smr/smr_init.m
@@ -1,3 +1,3 @@
 % 2010-11-10  Michele Tavella <michele.tavella@epfl.ch>
-mtpath_include('$EEGC3_INCLUDES/matndf');
+mtpath_include('$CNBITK_MATLAB/matndf');
 mtpath_include('$EEGC3_ROOT/modules/cl');
