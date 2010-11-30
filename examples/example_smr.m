279e1823fbc3cf938a732c81bbfe5329f9f87860
Michele Tavella <tavella.michele@gmail.com>
2010-11-30 14:37:01 +0100

    Added eegc3_smr_newevents

--- examples/example_smr.m
+++ examples/example_smr.m
@@ -1,7 +1,6 @@
 % 2010-11-26  Michele Tavella <michele.tavella@epfl.ch>
 mtpath_include('$EEGC3_ROOT/');					% Full eegc3
 mtpath_include('$EEGC3_ROOT/modules/smr');		% SMR module
-mtpath_include('$EEGC3_ROOT/modules/eegc2');	% eegc2 compatibility
 mtpath_include('$EEGC3_ROOT/modules/cl');		% libcnbiloop hooks 
 
 logfile = '~/Research/cnbi/mi/20101118_a2/';
---
+++ modules/smr/eegc3_smr_newevents.m
@@ -0,0 +1,27 @@
+% 2010-11-27  Michele Tavella <michele.tavella@epfl.ch>
+[cues, protocol, compat] = eegc3_smr_newevents()
+
+protocol.off 				= hex2dec('8000');
+protocol.inc 				= hex2dec('030a');
+protocol.trial 				= hex2dec('0300');
+protocol.trialoff 			= hex2dec('8300');
+protocol.cfeedback 			= hex2dec('030d');
+protocol.cfeedbackoff 		= hex2dec('830d');
+protocol.targethit 			= hex2dec('0381');
+protocol.targetmiss 		= hex2dec('0382');
+protocol.cross 				= hex2dec('0312');
+protocol.beep 				= hex2dec('0311');
+protocol.cueundef 			= hex2dec('030f');
+
+cues.right_hand_mi 			= hex2dec('0302');
+cues.left_hand_mi 			= hex2dec('0301');
+cues.both_hands_mi 			= hex2dec('0305');
+cues.both_feet_mi 			= hex2dec('0303');
+cues.rest_mi 				= hex2dec('030f');
+cues.tongue_mi 				= hex2dec('0304');
+cues.errp_detected 			= hex2dec('030b');
+cues.errp_notdetected 		= hex2dec('030c');
+cues.mi_onset_detected 		= hex2dec('010');
+cues.mi_onset_notdetected	= hex2dec('011');
+
+compat.protocol.trialoff 	= hex2dec('02F6');
