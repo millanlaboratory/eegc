ba2998fdc1c6fc8f5dcee982ea8c245d5f0f0416
Michele Tavella <tavella.michele@gmail.com>
2010-12-14 18:20:28 +0100

    Added conditional to switch off colorbar, thus saving space

--- featureselection/eegc3_dpplot.m
+++ featureselection/eegc3_dpplot.m
@@ -1,9 +1,13 @@
 % 2010-12-07  Michele Tavella <michele.tavella@epfl.ch>
-function eegc3_dpplot(f, M, channels, bands)
+function eegc3_dpplot(f, M, channels, bands, plotcb)
 
 if(nargin == 2)
 	channels = [1:1:16];
 	bands = [4:2:48];
+end
+
+if(nargin < 5)
+    plotcb = false;
 end
 
 eegc3_figure(f);
@@ -14,5 +18,7 @@ set(gca, 'XTick',      [1:1:length(bands)]);
 set(gca, 'XTickLabel', bands);
 xlabel('Band [Hz]');
 ylabel('Channel');
-colorbar;
 colormap gray;
+if(plotcb)
+    colorbar;
+end
