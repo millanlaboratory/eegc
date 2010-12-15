bf2144a7f4b491ac5ffb067408f76106f083145f
Michele Tavella <tavella.michele@gmail.com>
2010-12-15 23:08:27 +0100

    Added tool for converting Channels/Bands in indexes

--- featureselection/eegc3_dpmask.m
+++ featureselection/eegc3_dpmask.m
@@ -1,28 +1,19 @@
 % 2010-12-07  Michele Tavella <michele.tavella@epfl.ch>
-
 function [M, Mneg, Mall] = eegc3_dpmask(schannels, sbands, channels, bands)
 
-if(nargin == 2)
-	channels = [1:1:16];
-	bands = [4:2:48];
-end
-
 if(nargin == 1)
-	analysis = schannels;
-	channels = [1:1:analysis.settings.eeg.chs];
-	bands = analysis.settings.features.psd.freqs;
-	schannels = analysis.tools.features.channels;
-	sbands = analysis.tools.features.bands;
+	[ichannel, iband] = eegc3_chbn2idx(schannels);
+	channels = [1:1:schannels.settings.eeg.chs];
+	bands = schannels.settings.features.psd.freqs;
+else
+	[ichannel, iband] = eegc3_chbn2idx(schannels, sbands, channels, bands);
 end
 
 M = zeros(length(channels), length(bands));
 Mall = 0*M + 1;
-
-for c = schannels
-	freqs = sbands{c};
-	for f = freqs
-		M(c, find(bands == f)) = 1;
+for c = ichannel 
+	for b = iband 
+		M(c, b) = 1;
 	end
 end
-
 Mneg = -1*M + 1;
---
+++ tools/eegc3_chbn2idx.m
@@ -0,0 +1,24 @@
+% 2010-12-15  Michele Tavella <michele.tavella@epfl.ch> 
+function [ichannel, iband, labels] = eegc3_chbn2idx(schannels, sbands, channels, bands)
+
+ichannel = [];
+iband = [];
+labels = {};
+
+if(nargin == 1)
+	analysis = schannels;
+	channels = [1:1:analysis.settings.eeg.chs];
+	bands = analysis.settings.features.psd.freqs;
+	schannels = analysis.tools.features.channels;
+	sbands = analysis.tools.features.bands;
+end
+
+for c = schannels
+	freqs = sbands{c};
+	for f = freqs
+		b = find(bands == f);
+		ichannel(end+1) = c;
+		iband(end+1) = b;
+		labels{end+1} = sprintf('Ch. %d, %d Hz', c, b);
+	end
+end
