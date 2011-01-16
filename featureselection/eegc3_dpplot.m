686b0cb57af0aed65d114d6bed2060d75d1335fb
Michele Tavella <mtavella@menace.(none)>
2011-01-16 16:03:04 +0100

    Added interval as mandatory argument

--- featureselection/eegc3_dpplot.m
+++ featureselection/eegc3_dpplot.m
@@ -1,17 +1,17 @@
 % 2010-12-07  Michele Tavella <michele.tavella@epfl.ch>
-function eegc3_dpplot(f, M, channels, bands, plotcb)
+function eegc3_dpplot(f, M, interval, channels, bands, plotcb)
 
-if(nargin == 2)
+if(nargin == 3)
 	channels = [1:1:16];
 	bands = [4:2:48];
 end
 
-if(nargin < 5)
+if(nargin < 6)
     plotcb = false;
 end
 
 eegc3_figure(f);
-imagesc(M);%, [0 1]);
+imagesc(M, interval);
 set(gca, 'YTick',      [1:1:length(channels)]);
 set(gca, 'YTickLabel', channels);
 set(gca, 'XTick',      [1:1:length(bands)]);
