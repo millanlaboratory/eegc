14d7a5c3f5b501efdaea614523096a214f15eeaa
Michele Tavella <tavella.michele@gmail.com>
2011-01-27 22:54:49 +0100

    Now can work fine with gnome

--- visualization/eegc3_fullscreen.m
+++ visualization/eegc3_fullscreen.m
@@ -1,11 +1,15 @@
 % 2011-01-21  Michele Tavella <michele.tavella@epfl.ch>
-function eegc3_fullscreen(f)
+function eegc3_fullscreen(f, vp)
 
-if(nargin == 0)
+if(nargin < 1)
 	f = gcf();
+end
+
+if(nargin < 2)
+    vp = 0.75;
 end
 
 if(strcmp(get(gcf, 'WindowStyle'), 'docked') == 0)
     ss = get(0, 'ScreenSize');
-    set(f, 'Position', [0 0 ss(3) ss(4)]);
+    set(f, 'Position', [0 0 ss(3)*vp ss(4)*vp]);
 end
