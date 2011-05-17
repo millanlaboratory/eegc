17476c41019413b368435b158f47eb1052e91342
Michele Tavella <tavella.michele@gmail.com>
2011-05-17 18:55:25 +0200

    Updated CNBITKMAT_ROOT

--- modules/smr/smr_init.m
+++ modules/smr/smr_init.m
@@ -1,3 +1,3 @@
 % 2010-11-10  Michele Tavella <michele.tavella@epfl.ch>
-mtpath_include('$CNBITK_MATLAB/matndf');
+mtpath_include('$CNBITKMAT_ROOT/matndf');
 mtpath_include('$EEGC3_ROOT/modules/cl');
