a8e2422a231906928aca46049880f91dbf70c72f
Michele Tavella <tavella.michele@gmail.com>
2011-10-28 12:42:18 +0200

    Updated init file

--- eegc3_init.m
+++ eegc3_init.m
@@ -2,12 +2,10 @@
 % Import all directories but modules
 [file, basename] = mtpath_basename(which('eegc3_init'));
 
-mtpath_include('$EEGC3_ROOT/classification/');
 mtpath_include('$EEGC3_ROOT/configuration/');
 mtpath_include('$EEGC3_ROOT/dataset/');
 mtpath_include('$EEGC3_ROOT/featureextraction/');
 mtpath_include('$EEGC3_ROOT/featureselection/');
-mtpath_include('$EEGC3_ROOT/gui/');
 mtpath_include('$EEGC3_ROOT/inputoutput/');
 mtpath_include('$EEGC3_ROOT/integration/');
 mtpath_include('$EEGC3_ROOT/performance/');
